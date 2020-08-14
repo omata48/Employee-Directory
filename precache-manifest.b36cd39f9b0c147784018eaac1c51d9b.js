ee166bcfca2b447d20bd323b2547a06ba06a2b00
Omar <62365988+omata48@users.noreply.github.com>
2020-08-14 01:01:31 -0500

    Updates

---
+++ precache-manifest.b36cd39f9b0c147784018eaac1c51d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e431a4b14aab414df64d5ae091ee62",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "bdcdb724e4d2537aa857",
+    "url": "/Employee-Directory/static/css/main.6d64acd2.chunk.css"
+  },
+  {
+    "revision": "ade3c27a81c6fdd256e6",
+    "url": "/Employee-Directory/static/js/2.ab045e1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.ab045e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdcdb724e4d2537aa857",
+    "url": "/Employee-Directory/static/js/main.fe65a468.chunk.js"
+  },
+  {
+    "revision": "52e1e8e23e7dfaf64514",
+    "url": "/Employee-Directory/static/js/runtime-main.6ca9f082.js"
+  }
+]);
